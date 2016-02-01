77e345ad0ccd862f9ccbe7bdaac06aa793c636c0
Eric Field <ericdfield7@gmail.com>
2016-01-31 23:54:14 -0500

    Added functionality to draw lines between selected verticies

--- main.js
+++ main.js
@@ -22,6 +22,13 @@ fs.rename("C:/Users/Eric/human-vision/Images_2_Process/" + files3[0],"C:/Users/E
 Load = document.getElementById("Load");
 Load.onclick = function loadNewImage() {
 
+    // resets the select verticies clicks and removes any previous select verticies squares
+    counter=0;
+    var c = document.getElementById("layer2");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.clearRect(0, 0, layer2.width, layer2.height);
+
 fs.readdir("C:/Users/Eric/human-vision/Images_2_Process", function(err, files2) {
     if (err) throw err;
     console.log(files2); // gives an array of file names in folder
@@ -32,7 +39,7 @@ address2 = "C:/Users/Eric/human-vision/Images_2_Process/" + files2[0];
 newaddress2 = "C:/Users/Eric/human-vision/Processed_Images/" + files2[0];
 
 //document.getElementById("image").innerHTML = "<img src='" +address2+ "'>";
-   var img = new Image();
+var img = new Image();
 img.src = address2;
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
@@ -58,19 +65,20 @@ fs.rename("C:/Users/Eric/human-vision/Images_2_Process/" + files3[0],"C:/Users/E
 
 var coordinates = new Array();
 var counter = 0;
+
 document.getElementById("SelectVerticies").onclick = function SelectVerticies() {
     coordinates[counter]=document.addEventListener("dblclick", getClick, false);
 };
 function getClick(e) {
    
-      if (counter>=2)
+      if (counter>=16)
     {
         counter++;
         document.removeEventListener("dblclick",getClick,false);
         alert("removed");
     }
 
-    if (counter<2) {
+    if (counter<16) {
 
     var rect = document.getElementById("myCanvas").getBoundingClientRect();
         var x= e.clientX - rect.left;
@@ -81,7 +89,9 @@ function getClick(e) {
      ctx.fillStyle="#ff0000";
      ctx.fillRect(x,y,7,7);
      ctx.stroke();
-     coordinates[counter]=(x+","+y);
+     coordinates[counter]=x; //all even numbered values ie 0,2,4 are x coordinates
+     counter+=1;
+     coordinates[counter]=y; // all odd numbered values ie 1,3,5 are y coordinates
      console.log(coordinates);
 
      counter++;
@@ -96,6 +106,44 @@ clear.onclick = function clearLayer() {
     ctx.beginPath();
     ctx.clearRect(0, 0, layer2.width, layer2.height);
 }
+
+document.getElementById("Compute").onclick = function Compute() {
+
+    var numVerticies = coordinates.length;
+        
+    var c = document.getElementById("layer2");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    
+    if(numVerticies==6) {    
+        ctx.moveTo(coordinates[0],coordinates[1]);
+        ctx.lineTo(coordinates[2],coordinates[3]);
+        ctx.lineTo(coordinates[4],coordinates[5]);
+        ctx.lineTo(coordinates[0],coordinates[1]);
+        ctx.stroke();
+    }
+
+    if(numVerticies==8) {
+        ctx.moveTo(coordinates[0],coordinates[1]);
+        ctx.lineTo(coordinates[2],coordinates[3]);
+        ctx.lineTo(coordinates[4],coordinates[5]);
+        ctx.lineTo(coordinates[6],coordinates[7]);
+        ctx.lineTo(coordinates[0],coordinates[1]);
+        ctx.stroke();
+    }
+
+    if(numVerticies==10) {
+        ctx.moveTo(coordinates[0],coordinates[1]);
+        ctx.lineTo(coordinates[2],coordinates[3]);
+        ctx.lineTo(coordinates[4],coordinates[5]);
+        ctx.lineTo(coordinates[6],coordinates[7]);
+        ctx.lineTo(coordinates[8],coordinates[9]);
+        ctx.lineTo(coordinates[0],coordinates[1]);
+        ctx.stroke();
+    }
+
+
+};
 
 //Other tesed code snipits
 
