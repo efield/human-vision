22e767c989568505fcdc5a7fa69eb7ca151934ef
Eric Field <ericdfield7@gmail.com>
2016-02-07 00:30:12 -0500

    Added initial calculations for GPS location of selected point

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-//Variable Declarations;
+//Variable Declarations
 var fs = require("fs");
+var ExifImage = require("exif").ExifImage;
+var centerPixelWidth = 500;
+var centerPixelHeight = 320;
 var NoTarget;
 var Load;
 var address2;
@@ -51,27 +54,40 @@ Load = document.getElementById("Load").onclick = function loadNewImage()
 
             img.onload = function() 
             {
-                ctx.drawImage(img,0,0,1000,625);
+                ctx.drawImage(img,0,0,1000,640);
                 img.style.display = 'none';
             };
+
+            // Retrives metadata in image
+
+            new ExifImage({ image : address2 }, function (error, exifData) {
+                if (error)
+                    console.log("Error: "+error.message);
+                else
+                    console.log(exifData); // Do something with your data!
+                    console.log(exifData.makernote); // displays data creator attached to image ie. our alititude,latitiude etc...
+            });
         });
 
-        fs.readdir("../human-vision/Metadata", function(err, files4)
-        {
-            if (err) throw err;
-            console.log(files4); // gives an array of file names in folder
-            //console.log(files2.length); // displays number of files in the folder
+        // Reading metadata from .txt files
 
-            address4 = "../human-vision/Metadata/" + files4[0];
-        // Asynchronous read data from file into an array
-        fs.readFile(address4, "UTF-8", function (err, data) {
-           if (err) throw err;
-           {
-            imageData = data.split(","); // removes all "," from the string so "1,2,3" => "1","2","3"
-            console.log(imageData);
-            }
-        });
-        });
+        // fs.readdir("../human-vision/Metadata", function(err, files4)
+        // {
+        //     if (err) throw err;
+        //     console.log(files4); // gives an array of file names in folder
+        //     //console.log(files2.length); // displays number of files in the folder
+
+        //     address4 = "../human-vision/Metadata/" + files4[0];
+        
+        // // Asynchronous read data from file into an array
+        // fs.readFile(address4, "UTF-8", function (err, data) {
+        //    if (err) throw err;
+        //    {
+        //     imageData = data.split(","); // removes all "," from the string so "1,2,3" => "1","2","3"
+        //     console.log(imageData);
+        //     }
+        // });
+        // });
     };
 
 //If no target is selected the file is moved from the 'To process folder' to the 'Processed folder'
@@ -183,6 +199,64 @@ document.getElementById("Compute").onclick = function Compute()
             }
     };
 
+var lat1 = 50*Math.PI/180;
+var long1 = -90*Math.PI/180;
+var R = 6371 // km
+var altitude = 0.1 //km
+var initialHeading = 0; // rad
+var heading;
+
+var pixelW1 = 500;
+var pixelW2 = 320;
+
+var pixelH1 = 600;
+var pixelH2 = 350;
+
+var pixelWRad = 0.03146*Math.PI/180; // rad
+var pixelHRad = 0.02444*Math.PI/180; // rad
+
+var deltaW = .1; //altitude*Math.tan((pixelW2-pixelW1)*pixelWRad); // km
+var deltaH = .100; //altitude*Math.tan((pixelH2-pixelH1)*pixelHRad); // km
+
+var distance = Math.sqrt(deltaW*deltaW+deltaH*deltaH); // km
+
+    if (deltaW==0 && deltaH==0)
+    {
+        heading = initialHeading;
+        lat2 = lat1;
+        long2 = long1;
+    }
+    else if (deltaW>0 && deltaH<0)
+    {
+        heading = initialHeading + (Math.tan(deltaW/(-1*deltaH)))*Math.PI/180;
+        computeNewCoordinates(lat1,long1,distance,R,heading);
+    }
+    else if (deltaW>0 && deltaH>0)
+    {
+        heading = initialHeading + Math.PI - (Math.tan(deltaW/deltaH))*Math.PI/180;
+        computeNewCoordinates(lat1,long1,distance,R,heading);
+    }
+    else if (deltaW<0 && deltaH>0)
+    {
+        heading = initialHeading + Math.PI + (Math.tan((-1*deltaW)/deltaH))*Math.PI/180;
+        computeNewCoordinates(lat1,long1,distance,R,heading);
+    }
+    else if (deltaW<0 && deltaH<0)
+    {
+        heading = initialHeading + 2*Math.PI - (Math.tan(deltaW/deltaH))*Math.PI/180;
+        computeNewCoordinates(lat1,long1,distance,R,heading);
+    }
+
+var headingDeg = heading*180/Math.PI;
+alert("heading is "+headingDeg+" deg");
+
+function computeNewCoordinates(lat1,long1,distance,R,heading)
+{
+    var lat2= (Math.asin(Math.sin(lat1)*Math.cos(distance/R) + Math.cos(lat1)*Math.sin(distance/R)*Math.cos(heading)))*180/Math.PI;
+    var long2 = (long1 + Math.atan2(Math.sin(heading)*Math.sin(distance/R)*Math.cos(lat1), Math.cos(distance/R)-Math.sin(lat1)*Math.sin(lat2)))*180/Math.PI;
+    
+    alert(lat2+", "+long2);
+};
 //Other tesed code snipits
 
 //Works! Writes data to a file or writes data to a new file
